fd948fc6c72aa65f403ef73ceb0019e82136bcdc
Mike Chan <chanm003@gmail.com>
2019-09-17 20:42:32 +0200

    map CAC to users in database

--- api/config/passport.js
+++ api/config/passport.js
@@ -2,25 +2,42 @@ const CustomStrategy = require('passport-custom').Strategy;
 const session = require('express-session');
 const passport = require('passport');
 
-const { sessionKey } = require('./keys');
+const User = require('../models/user');
+const { isDevelopmentMode, sessionKey } = require('./keys');
 
 // export helper function that app.js can invoke
 module.exports = function (app) {
     passport.use('parseHttpHeader', new CustomStrategy(
         function (req, callback) {
-            console.log('Verification callback runs everytime passport.authenticate is invoked')
-            callback(null, { id: 1, name: 'Mike' });
+            const distinguishedName = req.headers['x-subject-dn'];
+            if (distinguishedName) {
+                User.mapToNewOrExistingUser(
+                    { distinguishedName },
+                    user => callback(null, user),
+                    err => callback(null, false, { err })
+                );
+            } else {
+                if (isDevelopmentMode) {
+                    console.log('DEV MODE no DN found in request header');
+                    User.mapToRandomUser(
+                        user => callback(null, user),
+                        err => callback(null, false)
+                    );
+                } else {
+                    console.log('PROD MODE no DN found in ');
+                    callback(null, false);
+                }
+            }
         }
     ));
 
     passport.serializeUser((user, done) => {
-        console.log('Serialization callback...runs ONCE when user successfully verifies')
-        done(null, user.id);
+        done(null, user._id);
     })
 
-    passport.deserializeUser((id, done) => {
-        console.log('Deserialize function...runs after every HTTP request AFTER user has authenticated')
-        done(null, { id: 1, name: 'Mike' });
+    passport.deserializeUser((_id, done) => {
+        User.findById(_id)
+            .then(user => done(null, user));
     })
 
     // middleware for Session
--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,51 @@
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-    distinguishedName: {
-        type: String,
-        required: true
+const schema = new mongoose.Schema(
+    {
+        distinguishedName: {
+            type: String,
+            required: true
+        },
+        lastLoggedIn: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        timestamps: true
     }
-});
+);
+
+schema.statics.mapToNewOrExistingUser = function (params, success, fail) {
+    const { distinguishedName } = params;
+    const update = { expire: new Date() };
+    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+    // create new user or update existing user
+    this.findOneAndUpdate({ distinguishedName }, update, options)
+        .then(newOrExistingUser => {
+            newOrExistingUser.lastLoggedIn = new Date();
+            newOrExistingUser.save()
+                .then(result => {
+                    success(result);
+                });
+        })
+        .catch(err => fail(err.message));
+}
+
+schema.statics.mapToRandomUser = function (success, fail) {
+    this.findOne()
+        .then(firstUser => {
+            if (firstUser) {
+                console.log('mapped to random user ', firstUser._id)
+                success(firstUser);
+            } else {
+                const message = 'Unable to assign random user since there are ZERO documents in the "users" collection';
+                console.log(message);
+                fail({ message });
+            }
+        })
+        .catch(err => fail(err.message));
+}
 
 module.exports = mongoose.model('User', schema);
