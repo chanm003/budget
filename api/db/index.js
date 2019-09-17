200676c05466bbf30d31b5264edac6561f2d0b1e
Mike Chan <chanm003@gmail.com>
2019-09-17 14:09:47 +0200

    move database connection logic to separate file

---
+++ api/db/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+module.exports = function (success, err) {
+    mongoose.set('useFindAndModify', false);
+
+    mongoose.connection.on("error", error => {
+        console.log("Database connection error:", error);
+    });
+
+    mongoose.connection.once("open", () => {
+        console.log("Connected to Database!");
+    });
+
+    mongoose.connect('mongodb://mongodb:27017/budget', { useNewUrlParser: true })
+        .then(() => {
+            success();
+        })
+        .catch(err => {
+            err(err);
+        });
+}
--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
 const graphqlHttp = require('express-graphql');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
+const connectToDatabase = require('./db/index');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,33 +14,25 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
+        return res.sendStatus(200);
     }
     next();
-});
-
-mongoose.set('useFindAndModify', false);
-mongoose.connection.on("error", error => {
-    console.log("Database connection error:", error);
-});
-
-mongoose.connection.once("open", () => {
-    console.log("Connected to Database!");
 });
 
 app.use(
     '/graphql',
     graphqlHttp({
-      schema: graphQlSchema,
-      rootValue: graphQlResolvers,
-      graphiql: true
+        schema: graphQlSchema,
+        rootValue: graphQlResolvers,
+        graphiql: true
     })
 );
 
-mongoose.connect('mongodb://mongodb:27017/budget', { useNewUrlParser: true })
-    .then(() => {
+connectToDatabase(
+    () => {
         app.listen(9000);
-    })
-    .catch(err => {
+    },
+    err => {
         console.log(err);
-    });
+    }
+);
