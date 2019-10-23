917427f747ad8bfd3fb096c84c13e403b381f5d8
Mike Chan <chanm003@gmail.com>
2019-10-23 18:01:16 +0200

    seed default user

--- api/seed/dataToSeed.js
+++ api/seed/dataToSeed.js
@@ -8,16 +8,18 @@ var chance = require('chance').Chance();
 const seeder = require('./seeder');
 const Directorate = require('../models/directorate');
 const Program = require('../models/program');
+const User = require('../models/user');
 
 const createItems = (num, createFunc) => {
     return Array.from({ length: num }).map(createFunc);
 }
 
 const createPrograms = num => {
-    return createItems(num, (item, index) => ( new Program({ title: `Program ${chance.radio()}` })));
+    return createItems(num, (item, index) => (new Program({ title: `Program ${chance.radio()}` })));
 }
 
 const dataToSeed = [
+    new User({ distinguishedName: 'CN=CHAN.MICHAEL.1175801169,OU=CONTRACTOR,OU=PKI,OU=DoD,O=U.S. Government,C=US' }),
     new Directorate({ title: 'SOJ2' }),
     new Directorate({ title: 'SOJ3' }),
     new Directorate({ title: 'SOJ6' }),
