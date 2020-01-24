0b3b8febbcf9721664d46a9fd7c5c7537ee13719
Mike Chan <chanm003@gmail.com>
2020-01-24 22:30:29 +0100

    Revert "remove husky"
    This reverts commit 882cfc127cd731eba7a928ebf88995d41c66105a.

--- api/src/config/express.ts
+++ api/src/config/express.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import compression from 'compression';
-<<<<<<< HEAD
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
@@ -17,13 +16,5 @@ export const setupExpress = () => {
     app.use(compression());
     app.use('/api/users', userRoutes);
     app.use(express.static('public'));
-=======
-import cors from 'cors';
-
-export const setupExpress = () => {
-    const app = express();
-    app.use('*', cors());
-    app.use(compression());
->>>>>>> 444d4e8372c3eb3ed71a021a0144e92f9907bbfc
     return app;
 };
--- api/src/data/database.ts
+++ api/src/data/database.ts
@@ -1,6 +1,5 @@
 import { connect } from 'mongoose';
 
-<<<<<<< HEAD
 import { seedDatabase } from './seed';
 
 export const connectToDatabase = async (
@@ -11,9 +10,4 @@ export const connectToDatabase = async (
     if (shouldSeedDatabase) {
         await seedDatabase();
     }
-=======
-export const connectToDatabase = async (shouldSeedData = true) => {
-    const mongoose = await connect('mongodb://mongodb:27017/budget');
-    await mongoose.connection.db.dropDatabase();
->>>>>>> 444d4e8372c3eb3ed71a021a0144e92f9907bbfc
 };
