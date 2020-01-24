882cfc127cd731eba7a928ebf88995d41c66105a
Mike Chan <chanm003@gmail.com>
2020-01-24 21:24:13 +0100

    remove husky

--- api/src/data/seed.ts
+++ api/src/data/seed.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Chance from 'chance';
 import {
     Directorate,
@@ -41,5 +42,42 @@ export async function seedDatabase() {
 
     return {
         defaultUser: defaultUser,
+=======
+import { Recipe, RecipeModel } from './entities/recipe';
+import { User, UserModel } from './entities/user';
+
+export async function seedDatabase() {
+    const defaultUser = await new UserModel({
+        email: 'test@github.com',
+        nickname: 'MichalLytek',
+        password: 's3cr3tp4ssw0rd',
+    } as User).save();
+
+    await RecipeModel.create([
+        {
+            title: 'Recipe 1',
+            description: 'Desc 1',
+            author: defaultUser._id,
+            ratings: [
+                { value: 2, user: defaultUser._id },
+                { value: 4, user: defaultUser._id },
+                { value: 5, user: defaultUser._id },
+                { value: 3, user: defaultUser._id },
+                { value: 4, user: defaultUser._id },
+            ],
+        },
+        {
+            title: 'Recipe 2',
+            author: defaultUser._id,
+            ratings: [
+                { value: 2, user: defaultUser },
+                { value: 4, user: defaultUser },
+            ],
+        },
+    ] as Recipe[]);
+
+    return {
+        defaultUser,
+>>>>>>> 444d4e8372c3eb3ed71a021a0144e92f9907bbfc
     };
 }
