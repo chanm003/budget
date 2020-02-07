ef7ffff469832fe188f871b8c1e3b810c19515d1
Mike Chan <chanm003@gmail.com>
2020-02-07 13:39:13 +0100

    refactor seed logic

--- api/src/data/seed.ts
+++ api/src/data/seed.ts
@@ -1,16 +1,49 @@
 import Chance from 'chance';
+import _ from 'lodash';
 import {
     Directorate,
     DirectorateModel,
 } from './entities/directorate/model';
+import { Program, ProgramModel } from './entities/program/model';
 import { User, UserModel } from './entities/user/model';
 const { roleNames } = require('shared');
 import { hashPassword } from '../config/password';
 
+const chance = new Chance();
+
 export async function seedDatabase() {
-    const chance = new Chance();
+    const defaultUser = await createFirstUser();
+    const directorates = await createDirectorates(defaultUser);
+
+    return {
+        user: defaultUser,
+        directorates,
+    };
+}
+
+async function createDirectorates(user: User) {
+    return await DirectorateModel.create([
+        {
+            title: 'SOJ3',
+            createdBy: user._id,
+            updatedBy: user._id,
+        },
+        {
+            title: 'SOJ6',
+            createdBy: user._id,
+            updatedBy: user._id,
+        },
+        {
+            title: 'SOJ69',
+            createdBy: user._id,
+            updatedBy: user._id,
+        },
+    ] as Directorate[]);
+}
+
+async function createFirstUser() {
     const password = await hashPassword('12345678');
-    const defaultUser = await new UserModel({
+    return await new UserModel({
         firstName: 'John',
         lastName: chance.last({ nationality: 'en' }),
         email: 'jdoe@gmail.com',
@@ -20,27 +53,4 @@ export async function seedDatabase() {
         },
         role: roleNames.ADMIN,
     } as User).save();
-
-    const directorates = await DirectorateModel.create([
-        {
-            title: 'SOJ3',
-            createdBy: defaultUser._id,
-            updatedBy: defaultUser._id,
-        },
-        {
-            title: 'SOJ6',
-            createdBy: defaultUser._id,
-            updatedBy: defaultUser._id,
-        },
-        {
-            title: 'SOJ69',
-            createdBy: defaultUser._id,
-            updatedBy: defaultUser._id,
-        },
-    ] as Directorate[]);
-
-    return {
-        user: defaultUser,
-        directorates,
-    };
 }
